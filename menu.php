4b8a18c5c7018ad716aa86457153327b34ccefe2
matildessantos <matildesantos13@gmail.com>
2021-06-23 22:28:22 +0100

    header - menu

--- menu.php
+++ menu.php
@@ -7,20 +7,20 @@
     <div class="header-menu">
     
         <nav class="row menu">
-            <ul class="col-10 container">
+            <ul class="container">
                 <li class="box">ABOUT KRUGER</li>
                 <li class="box">PLAN YOUR VISIT</li> 
                 <li class="box">GET INVOLVED</li>
                 <li class="box">STORE</li>
             </ul>
             
-            <ul class="col-1">
+            <ul class="linguas">
                 <li class="box">EN</li>
             </ul>
 
-            <ul class="col-1">    
-                <li class="box"><i class="fa fa-user"></i></li>
-                <li class="box"><i class="fa fa-shopping-cart"></i></li>
+            <ul class="itens">    
+                <li class=" col-6 item"><i class="fa fa-user"></i></li>
+                <li class="col-6 item"><i class="fa fa-shopping-cart"></i></li>
 
             </ul>
 
