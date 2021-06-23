37d4ffc27fb77896dbeb81ca63409dba6d2e7104
matildessantos <matildesantos13@gmail.com>
2021-06-23 19:46:10 +0100

    fonts

--- index.php
+++ index.php
@@ -1,42 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>s
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="../fonts/stylesheet.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 
     <header class="col-12 offset-0 header">
 
-        <div class="languages">
-            PT
-        </div>
-
-        <div class="header-menu">
-            <div class="logo">
-
-            </div>
-
-            <nav class="menu">
-                <ul class="container">
-                    <li class="box">Novo</li>
-                    <li class="box">Roupa</li> 
-                    <li class="box">Acessórios</li>
-                    <li class="box">Sapatos</li>
-                    <li class="box">Sugestões</li>
-                </ul>
-
-            </nav>
-        </div>
-
-        <div class="items">
-
-        </div>
+        <?php include('menu.php'); ?>
 
     </header>
 
---
+++ menu.php
@@ -0,0 +1,28 @@
+<header class="col-12 offset-0 header">
+
+    <div class="languages">
+        PT
+    </div>
+
+    <div class="header-menu">
+        <div class="logo">
+
+        </div>
+
+        <nav class="menu">
+            <ul class="container">
+                <li class="box">Novo</li>
+                <li class="box">Roupa</li> 
+                <li class="box">Acessórios</li>
+                <li class="box">Sapatos</li>
+                <li class="box">Sugestões</li>
+            </ul>
+
+        </nav>
+    </div>
+
+    <div class="items">
+
+    </div>
+
+</header>
